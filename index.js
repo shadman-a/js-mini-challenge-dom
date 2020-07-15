788586256792dcfa14c6cfbdff4729bae919a785
shadman-a <shadman@outlook.com>
2020-07-14 23:54:46 -0400

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+header.style.color = "red";
 
 /***** Deliverable 3 *****/
 // console.log("PLAYERS array looks like this:", PLAYERS)
+playerDiv = document.querySelector(".player-container")
 
+PLAYERS.forEach(player => {
+    playerDiv.innerHTML += `<div class="player" data-number="${player.number}"><h3>
+      ${player.name} (<em>${player.nickname}</em>)
+    </h3>
+    <img src="${player.photo}" alt="${player.name}">
+  </div>`
+});
 
 /***** Deliverable 4 *****/
+const person = document.querySelector(`[data-number="7"]`)
+
+// console.log(person)
+person.remove()
